a8d070acb73675b9747b15ebe8dd73b0b71cac80
FHGDev <gideon.lingle1@gmail.com>
2018-12-17 11:39:10 -0600

    Update wallpost.js

--- commands/wallpost.js
+++ commands/wallpost.js
@@ -1,1 +1,11 @@
-module.exports.help = {name: ""}
+function runCmd(bot, message, args) {
+  require('roblox-js').post(bot.groupId, args.join(" "));
+  
+  message.channel.send(`I posted ${args.join(" ")} to the group wall.`)
+}
+
+module.exports = {
+  name: "wallpost",
+  perm: "Admin",
+  run: runCmd
+}
